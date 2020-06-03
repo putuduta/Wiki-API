f8f591628de6ac0d541bea2f5e2f49a5aba2c756
putuduta <putuduta@gmail.com>
2020-06-03 11:28:15 +0700

    Complete Wiki-API

--- app.js
+++ app.js
@@ -29,39 +29,127 @@ const articleSchema = {
 //create article model
 const Article = mongoose.model("Article", articleSchema);
 
-//get method => fetches all the articles
-app.get('/articles', (req, res) => {
+/*
+    Requests Targetting  all articles
+*/
 
-    //find the articles
-    Article.find((err, foundArticles) => {
+app.route('/articles')
+    //get method => fetches all the articles
+    .get((req, res) => {
 
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
+        //find the articles
+        Article.find((err, foundArticles) => {
+
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .post((req, res) => {
+
+        //cretae the article
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        //save the article
+        newArticle.save((err) => {
+
+            if (!err) {
+                res.send('Successfully added a new article.');
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .delete((req, res) => {
+
+        //delete all the articles
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send('Succesfully delete all the articles');
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
 
-//creates one onew article
-app.post('/articles', (req, res) => {
+/*
+    Requests Targetting  a specific articles
+*/
 
-    //cretae the article
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+app.route('/articles/:articleTitle')
+
+    //get a specific article
+    .get((req, res) => {
+
+        Article.findOne({
+            title: req.params.articleTitle
+        }, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send('No article was found');
+            }
+        });
+    })
+
+    //update a specific article
+    // => replace the entire document
+    .put((req, res) => {
+
+        Article.update({
+            title: req.params.articleTitle
+        }, {
+            title: req.body.title,
+            content: req.body.content
+        }, {
+            overwrite: true
+        }, (err) => {
+
+            if (!err) {
+                res.send('Succesfully update the article');
+            }
+        });
+    })
+
+    //update a specific article
+    // => only replace a specific field
+    .patch((req, res) => {
+
+        Article.update({
+            title: req.params.articleTitle
+        }, {
+            //pick out the field that already provided
+            $set: req.body
+        }, (err) => {
+
+            if (!err) {
+                res.send('Successfully updated article');
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    //delete a specific article
+    .delete((req, res) => {
+
+        Article.deleteOne({
+            title: req.params.articleTitle
+        }, (err) => {
+            if (!err) {
+                res.send('Successfully deleted the corresponding article');
+            } else {
+                res.send(err);
+            }
+        });
     });
-
-    //save the article
-    newArticle.save((err) => {
-
-        if (!err) {
-            res.send('Successfully added a new article.');
-        } else {
-            res.send(err);
-        }
-    });
-});
 
 //listen to port 3000
 app.listen(3000, () => {
